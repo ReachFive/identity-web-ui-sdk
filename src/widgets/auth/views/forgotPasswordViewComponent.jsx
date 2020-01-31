b7e15f0b46a1d4829e5c0ee4ad8a09a6067487a6
Guillaume Bersac <bersac_1@hotmail.fr>
2020-01-31 16:09:12 +0100

    update readme to explain how to test local version of sdk ui

--- src/widgets/auth/views/forgotPasswordViewComponent.jsx
+++ src/widgets/auth/views/forgotPasswordViewComponent.jsx
@@ -24,7 +24,7 @@ const ForgotPasswordForm = createForm({
 const skipError = err => err.error === 'resource_not_found';
 
 const enhance = withHandlers({
-    handleSubmit: ({ apiClient, redirectUrl, finalRedirectUri }) => data => apiClient.requestPasswordReset({ ...data, redirectUrl, finalRedirectUri })
+    handleSubmit: ({ apiClient, redirectUrl, returnToAfterResetPassword }) => data => apiClient.requestPasswordReset({ ...data, redirectUrl, returnToAfterResetPassword })
 });
 
 export const ForgotPasswordView = enhance(({ i18n, goTo, allowLogin, handleSubmit, displaySafeErrorMessage, showLabels }) => (
