eef41c56a54536bff907dc7b3283098362a82a75
Roxane Macé <roxane.mace@outlook.com>
2020-06-05 10:15:07 +0200

    Update the links to go to the webauthn login view

--- src/widgets/auth/views/quickLoginViewComponent.jsx
+++ src/widgets/auth/views/quickLoginViewComponent.jsx
@@ -5,14 +5,14 @@ import SocialButtons from '../../../components/form/socialButtonsComponent';
 
 export default class QuickLoginView extends React.Component {
     render() {
-        const { i18n, session = {} } = this.props;
+        const { i18n, session = {}, webAuthn } = this.props;
 
         return <div>
             <Heading>{session.name || session.email}</Heading>
             <Intro>{i18n('lastTimeYouLoggedInWith')}</Intro>
             <SocialButtons providers={[session.lastLoginType]} auth={this.props.auth} />
             <Alternative>
-                <Link target="login">{i18n('notYourAccount')}</Link>
+                <Link target={webAuthn ? 'login-with-web-authn' : 'login'}>{i18n('notYourAccount')}</Link>
             </Alternative>
         </div>;
     }
