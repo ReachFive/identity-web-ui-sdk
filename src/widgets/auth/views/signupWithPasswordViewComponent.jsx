39be811f618ad2675b6a529099555bedd1a64eb5
Roxane Macé <roxane.mace@outlook.com>
2020-09-25 09:54:19 +0200

    Redirect to the associated view

---
+++ src/widgets/auth/views/signupWithPasswordViewComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Heading } from '../../../components/miscComponent';
+
+
+export default class SignupWithPasswordView extends React.Component {
+    render() {
+        return <div>
+            <Heading>Signup with password</Heading>
+        </div>;
+    }
+}
---
+++ src/widgets/auth/views/signupWithWebAuthnViewComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Heading } from '../../../components/miscComponent';
+
+
+export default class SignupWithWebAuthnView extends React.Component {
+    render() {
+        return <div>
+            <Heading>Signup with FIDO2</Heading>
+        </div>;
+    }
+}
