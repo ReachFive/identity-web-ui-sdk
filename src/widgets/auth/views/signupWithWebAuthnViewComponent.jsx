da3d8e6e8bf7d35884bf94312009a2ba3dd88011
Roxane Macé <roxane.mace@outlook.com>
2020-09-25 14:18:01 +0200

    Build the FIDO2 signup form

--- src/widgets/auth/views/signupWithWebAuthnViewComponent.jsx
+++ src/widgets/auth/views/signupWithWebAuthnViewComponent.jsx
@@ -1,14 +1,60 @@
 import React from 'react';
 
-import { Heading, Link } from '../../../components/miscComponent';
-import MultiSignupForm from '../../../components/form/multiSignupFormComponent';
+import { createForm } from '../../../components/form/formComponent';
+import { buildFormFields } from '../../../components/form/formFieldFactory';
+import { Heading, Link, MarkdownContent } from '../../../components/miscComponent';
+import { snakeCaseProperties } from '../../../helpers/transformObjectProperties';
 
+const defaultSignupFields = [
+    'given_name',
+    'family_name',
+    'email'
+];
+
+const SignupForm = createForm({
+    prefix: 'r5-webauthn-signup-',
+    submitLabel: 'signup.submitLabel'
+});
 
 export default class SignupWithWebAuthnView extends React.Component {
+    handleSignup = data => this.props.apiClient.signupWithWebAuthn(
+        {
+            profile: snakeCaseProperties(data),
+            friendlyName: '',
+            redirectUrl: this.props && this.props.redirectUrl
+        },
+        this.props.auth
+    );
+
     render() {
+        const {
+            beforeSignup = x => x,
+            signupFields = defaultSignupFields,
+            userAgreement
+        } = this.props;
+
+        // The password fields shouldn't be in the webauthn signup even if they are provided in the options
+        const filteredSignupFields = signupFields.filter(field => field !== 'password' && field !== 'password_confirmation')
+
+        const fields = buildFormFields(filteredSignupFields, this.props.config);
+
+        const allFields = userAgreement
+        ? [
+            ...fields,
+            {
+                staticContent: <MarkdownContent key="user-aggreement" root={UserAggreementStyle} source={userAgreement} />
+            }
+        ]
+        : fields;
+
+
         return <div>
             <Heading>Signup with FIDO2</Heading>
-            <MultiSignupForm {...this.props} />
+            <SignupForm
+                fields={allFields}
+                showLabels={this.props.showLabels}
+                beforeSubmit={beforeSignup}
+                handler={this.handleSignup} />
             <Link target={'signup'}>Back</Link>
         </div>;
     }
