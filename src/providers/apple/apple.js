ef8ce6f0ef7d5abb09e7d2c2907d4b3b2c64ba99
Roxane Macé <roxane.mace@outlook.com>
2020-12-01 18:17:45 +0100

    Change the colour of the Apple button

--- src/providers/apple/apple.js
+++ src/providers/apple/apple.js
@@ -3,7 +3,7 @@ import icon from './apple.svg';
 export default {
     key: 'apple',
     name: 'Sign in with Apple',
-    color: '#333',
+    color: '#000',
     fontWeight: 400,
     fontFamily: 'SF Pro Text,SF Pro Icons,Helvetica Neue,Helvetica,Arial,sans-serif',
     icon
