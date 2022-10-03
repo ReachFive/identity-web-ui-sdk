dd9c579aa823f26a011afc9a5f0ca5396c7d2d80
sacha <sacha.memmi@reach5.co>
2022-10-03 13:47:12 +0200

    Default value when options length is 1

--- src/components/form/fields/radioboxField.jsx
+++ src/components/form/fields/radioboxField.jsx
@@ -11,6 +11,7 @@ const RadioboxField = props => {
 export default function radioboxField({options, ...config}) {
     return createField({
         ...config,
+        defaultValue: options.length === 1 ? options[0].value: undefined,
         component: RadioboxField,
         extendedParams: i18n => ({
             options: options.map(({ label, value, key }) => ({ value, label: i18n(label || value), key}))
