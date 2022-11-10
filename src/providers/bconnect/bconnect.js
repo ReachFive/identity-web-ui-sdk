107b85a5164bc96315c4506a15af7998412c4bca
Quentin Aupetit <quentin.aupetit@reach5.co>
2022-11-10 10:28:31 -0400

    add B.connect provider

---
+++ src/providers/bconnect/bconnect.js
@@ -0,0 +1,11 @@
+import icon from './bconnect.svg';
+
+export default {
+    key: 'bconnect',
+    name: 'B.connect',
+    color: '#ff4f00',
+    btnBorderColor: 'rgba(0,0,0,.15)',
+    btnTextColor: '#58666e',
+    btnBackgroundColor: '#ffffff',
+    icon,
+};
