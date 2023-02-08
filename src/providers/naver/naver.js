01a1cf04cb2dd89ea7d14a07ed9b7baff7d8af5e
sacha <sacha.memmi@reach5.co>
2023-02-08 17:28:59 +0100

    Add social provider Naver

---
+++ src/providers/naver/naver.js
@@ -0,0 +1,12 @@
+import icon from './naver.svg';
+
+export default {
+    key: 'naver',
+    name: 'Naver',
+    color: '#19CE60',
+    icon,
+    windowSize: {
+        width: 450,
+        height: 400
+    }
+};
