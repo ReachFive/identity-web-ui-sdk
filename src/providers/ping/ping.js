ee6138e51b8c826957016ca971f5b172d57fed2c
rchapel <50823416+rchapel@users.noreply.github.com>
2023-10-11 18:06:30 +0200

    [CA-3078] Add Ping provider (#153)

---
+++ src/providers/ping/ping.js
@@ -0,0 +1,12 @@
+import icon from './ping.svg';
+
+export default {
+    key: 'ping',
+    name: 'Ping',
+    color: '#c90917',
+    icon,
+    windowSize: {
+        width: 450,
+        height: 400
+    }
+};
