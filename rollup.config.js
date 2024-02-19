740b2caea039933772c681314db47fdc9723ced4
Quentin AUPETIT <quentin.aupetit@reach5.co>
2024-02-19 13:15:08 -0400

    fix umd deps embeding

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,49 @@
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import url from '@rollup/plugin-url'
 import svg from '@svgr/rollup'
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 
+import pkg from './package.json' assert { type: "json" }
+const dependencies = Object.keys(pkg.dependencies)
+
+// Ignore Luxon library's circular dependencies
+function onWarn(message) {
+    if ( message.code === 'CIRCULAR_DEPENDENCY' ) return;
+    console.warn( message);
+}
+
 const bundle = config => ({
     ...config,
     input: 'src/index.ts',
-    external: id => !/^[./]/.test(id),
 })
 
-const globals = {
-    '@reachfive/identity-core': 'identityCore',
-    '@reachfive/zxcvbn': 'zxcvbn',
-    'react': 'React',
-    'react-dom/client': 'client',
-    'styled-components': 'styled',
-    'polished': 'polished',
-    'react-transition-group': 'react-transition-group',
-    'classnames': 'classes',
-    'remarkable': 'remarkable',
-    'libphonenumber-js': 'libphonenumber-js',
-    'validator': 'validator',
-    'luxon': 'luxon',
-    'char-info': 'char-info',
-    'lodash-es': 'lodash-es',
-    'lodash-es/camelCase': 'lodash-es/camelCase',
-    'lodash-es/snakeCase': 'lodash-es/snakeCase',
-    'lodash-es/isFunction': 'lodash-es/isFunction',
-    'lodash-es/some': 'lodash-es/some',
-    'lodash-es/compact': 'lodash-es/compact',
-    'lodash-es/debounce': 'lodash-es/debounce',
-    'lodash-es/pick': 'lodash-es/pick',
-    'lodash-es/isEmpty': 'lodash-es/isEmpty',
-    'lodash-es/isString': 'lodash-es/isString',
-    'lodash-es/find': 'lodash-es/find',
-    'lodash-es/intersection': 'lodash-es/intersection',
-    'lodash-es/difference': 'lodash-es/difference',
-}
+const plugins = [
+    replace({
+        preventAssignment: true,
+        values: {
+            'process.env.NODE_ENV': JSON.stringify('production'),
+            'lodash/': 'lodash-es/',
+        }
+    }),
+    nodeResolve({
+        extensions: ['.jsx', '.js', '.json'],
+        preferBuiltins: true
+    }),
+    commonjs({ include: /node_modules/ }),
+    svg(),
+    // Add an inlined version of SVG files: https://www.smooth-code.com/open-source/svgr/docs/rollup/#using-with-url-plugin
+    url({ limit: Infinity, include: ['**/*.svg'] }),
+    esbuild(),
+]
 
 export default [
     bundle({
-        plugins: [
-            svg(),
-            // Add an inlined version of SVG files: https://www.smooth-code.com/open-source/svgr/docs/rollup/#using-with-url-plugin
-            url({ limit: Infinity, include: ['**/*.svg'] }),
-            esbuild(),
-        ],
+        plugins,
+        external: dependencies,
         output: [
             {
                 file: 'cjs/identity-ui.js',
@@ -64,12 +61,18 @@ export default [
                 plugins: [terser()],
                 sourcemap: true,
             },
+        ],
+        onwarn: onWarn
+    }),
+    bundle({
+        plugins,
+        output: [
             {
                 file: 'umd/identity-ui.js',
                 format: 'umd',
                 name: 'IdentityUI',
                 sourcemap: true,
-                globals
+                globals: { '@reachfive/identity-core': 'reach5' },
             },
             {
                 file: 'umd/identity-ui.min.js',
@@ -77,9 +80,10 @@ export default [
                 name: 'IdentityUI',
                 plugins: [terser()],
                 sourcemap: true,
-                globals
-            }
+                globals: { '@reachfive/identity-core': 'reach5' },
+            },
         ],
+        onwarn: onWarn
     }),
     bundle({
         plugins: [
@@ -89,5 +93,6 @@ export default [
             file: 'types/identity-ui.d.ts',
             format: 'es',
         },
+        onwarn: onWarn
     }),
 ]
