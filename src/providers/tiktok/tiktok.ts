cbab746b817741669f3791dd5aef56f426cd9f2b
sachaMemmir5 <93584845+sachaMemmir5@users.noreply.github.com>
2024-05-06 13:04:07 +0200

    [CA-3569] Add tiktok social provider (#205)

---
+++ src/providers/tiktok/tiktok.ts
@@ -0,0 +1,14 @@
+import icon from './tiktok.svg'
+
+import type { Provider } from '../providers'
+
+export default {
+    key: 'tiktok',
+    name: 'TikTok',
+    color: '#000',
+    icon,
+    windowSize: {
+        width: 440,
+        height: 550
+    }
+} satisfies Provider;
