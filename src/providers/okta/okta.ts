55fbbef6f01066cf032b8053f0bcdfe7123060ef
Romain Chapel <romain.chapel@reach5.co>
2024-07-03 16:02:28 +0200

    Update color

--- src/providers/okta/okta.ts
+++ src/providers/okta/okta.ts
@@ -1,11 +1,11 @@
-import icon from './okta.svg.svg';
+import icon from './okta.svg';
 
 import type { Provider } from '../providers'
 
 export default {
     key: 'okta',
     name: 'Okta',
-    color: '#4f3d3e',
+    color: '#a4a4a491',
     icon,
     windowSize: {
         width: 450,
