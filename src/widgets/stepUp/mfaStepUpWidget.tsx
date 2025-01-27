3a6a73f0decbaa081432f6c99b755856676198e5
sacha <sacha.memmi@reach5.co>
2025-01-27 16:03:46 +0100

    Add trust device checkbox

--- src/widgets/stepUp/mfaStepUpWidget.tsx
+++ src/widgets/stepUp/mfaStepUpWidget.tsx
@@ -15,25 +15,39 @@ import { toQueryString } from '../../helpers/queryString';
 import { useReachfive } from '../../contexts/reachfive';
 import { useRouting } from '../../contexts/routing';
 import { useI18n } from '../../contexts/i18n';
+import checkboxField from "../../components/form/fields/checkboxField";
+import { useConfig } from "../../contexts/config.tsx";
 
 const StartStepUpMfaButton = createForm({
     prefix: 'r5-mfa-start-step-up-',
     submitLabel: 'mfa.stepUp.start',
 });
 
-type VerificationCodeInputFormData = {
+export type VerificationCodeInputFormData = {
     verificationCode: string
+    trustDevice?: boolean
 }
 
-const VerificationCodeInputForm = createForm<VerificationCodeInputFormData>({
+interface VerificationCodeFormOptions {
+    allowTrustDevice?: boolean
+}
+
+const VerificationCodeInputForm = createForm<VerificationCodeInputFormData, VerificationCodeFormOptions>({
     prefix: 'r5-passwordless-sms-',
-    fields: [
-        simpleField({
-            key: 'verification_code',
-            label: 'verificationCode',
-            type: 'text'
-        })
-    ]
+    fields({allowTrustDevice }) {
+        return [
+            simpleField({
+                key: 'verification_code',
+                label: 'verificationCode',
+                type: 'text'
+            }),
+        ...(allowTrustDevice ? [ checkboxField({
+                key: 'trust_device',
+                label: 'mfa.stepUp.trustDevice',
+                defaultValue: false
+            })] : [])
+        ]
+    }
 });
 
 type StartPasswordlessFormData = {
@@ -60,9 +74,9 @@ const StartPasswordlessForm = createForm<StartPasswordlessFormData, StartPasswor
 export interface MainViewProps {
     /**
      * **Not recommended**
-     * 
+     *
      * The authorization credential JSON Web Token (JWT) used to access the ReachFive API, less than five minutes old.
-     * 
+     *
      * If empty, using an existing SSO session cookie.
      */
     accessToken?: string
@@ -72,19 +86,26 @@ export interface MainViewProps {
     auth?: AuthOptions
     /**
      * Show the introduction text.
-     * 
+     *
      * @default true
      */
     showIntro?: boolean
     /**
      * Show the stepup button. Unnecessary for console use
-     * 
+     *
      * @default true
      */
     showStepUpStart?: boolean
+    /**
+     * Boolean that specifies whether a device can be trusted during step up.
+     *
+     * @default false
+     */
+
+    allowTrustDevice?: boolean
 }
 
-export const MainView = ({ accessToken, auth, showIntro = true, showStepUpStart = true }: MainViewProps) => {
+export const MainView = ({ accessToken, auth, showIntro = true, showStepUpStart = true, allowTrustDevice = false }: MainViewProps) => {
     const coreClient = useReachfive()
     const { goTo } = useRouting()
 
@@ -113,23 +134,26 @@ export const MainView = ({ accessToken, auth, showIntro = true, showStepUpStart 
         return (
             <StartStepUpMfaButton
                 handler={onGetStepUpToken}
-                onSuccess={(data: MFA.StepUpResponse) => goTo<FaSelectionViewState>('fa-selection', { ...data })}
+                onSuccess={(data: MFA.StepUpResponse) => goTo<FaSelectionViewState>('fa-selection', { ...data, allowTrustDevice})}
             />
         )
     }
 
     if (response) {
-        return <FaSelectionView {...response} showIntro={showIntro} auth={auth} />
+        return <FaSelectionView {...response} showIntro={showIntro} auth={auth} allowTrustDevice={allowTrustDevice}/>
     }
 
     return null
 }
 
-export type FaSelectionViewState = MFA.StepUpResponse
+export type FaSelectionViewState = MFA.StepUpResponse & {
+    allowTrustDevice?: boolean
+}
 
 export type FaSelectionViewProps = Prettify<Partial<MFA.StepUpResponse> & {
     showIntro?: boolean
     auth?: AuthOptions
+    allowTrustDevice?: boolean
 }>
 
  // Unlike single factor authentication, StepUp request always returns a challengeId
@@ -165,7 +189,7 @@ export const FaSelectionView = (props: FaSelectionViewProps) => {
     }, [amr, onChooseFa])
 
     if (response) {
-        return <VerificationCodeView {...response} auth={props.auth} />
+        return <VerificationCodeView {...response} auth={props.auth} allowTrustDevice={props.allowTrustDevice}/>
     }
 
     if (amr.length > 1) {
@@ -190,27 +214,34 @@ export type VerificationCodeViewProps = Prettify<Partial<StepUpHandlerResponse> 
      * List of authentication options
      */
     auth?: AuthOptions
+    /**
+     * Boolean that specifies whether a device can be trusted during step up.
+     *
+     * @default false
+     */
+    allowTrustDevice?: boolean
 }>
 
 export const VerificationCodeView = (props: VerificationCodeViewProps) => {
     const coreClient = useReachfive()
     const i18n = useI18n()
     const { params } = useRouting()
+    const { rbaEnabled} = useConfig()
     const state = params as VerificationCodeViewState
 
-    const { auth, authType, challengeId } = { ...props, ...state }
+    const { auth, authType, challengeId, allowTrustDevice } = { ...props, ...state }
 
     const handleSubmit = (data: VerificationCodeInputFormData) =>
         coreClient
-            .verifyMfaPasswordless({challengeId, verificationCode: data.verificationCode})
-            // @ts-expect-error AuthResult is too complex and is not representative of the real response of this request 
+            .verifyMfaPasswordless({challengeId, verificationCode: data.verificationCode, trustDevice: data.trustDevice})
+            // @ts-expect-error AuthResult is too complex and is not representative of the real response of this request
             .then(resp => window.location.replace( auth?.redirectUri + "?" + toQueryString(resp)))
 
     return (
         <div>
             {authType === 'sms' && <Info>{i18n('passwordless.sms.verification.intro')}</Info>}
             {authType === 'email' && <Info>{i18n('passwordless.email.verification.intro')}</Info>}
-            <VerificationCodeInputForm handler={handleSubmit}/>
+            <VerificationCodeInputForm allowTrustDevice={rbaEnabled && allowTrustDevice} handler={handleSubmit}/>
         </div>
     )
 }
