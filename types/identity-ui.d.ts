9d69a27c0ce708ff62164abf9f0cad10ad51dc42
Quentin Aupetit <qaupetit@gmail.com>
2025-02-11 15:11:21 -0400

    upgrade styled-components to v5.3.11 (#262)

--- types/identity-ui.d.ts
+++ types/identity-ui.d.ts
@@ -1,6 +1,6 @@
 /**
- * @reachfive/identity-ui - v1.32.0
- * Compiled Thu, 06 Feb 2025 14:40:46 UTC
+ * @reachfive/identity-ui - v1.32.2
+ * Compiled Tue, 11 Feb 2025 18:05:42 UTC
  *
  * Copyright (c) ReachFive.
  *
