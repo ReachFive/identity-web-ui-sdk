392cb1c9ee12b937753bd165fd0945baf13423a3
sacha <sacha.memmi@reach5.co>
2025-02-27 13:53:14 +0100

    npm run test:update

--- tests/widgets/mfa/mfaListWidget.test.ts
+++ tests/widgets/mfa/mfaListWidget.test.ts
@@ -70,7 +70,7 @@ describe('Snapshot', () => {
     test('empty', generateSnapshot({}, undefined, []))
 
     test('basic', generateSnapshot({}, undefined, [
-        { type: 'sms', phoneNumber: '33612345678', friendlyName: 'identifier', createdAt: '2022-09-21' },
+        { type: 'sms', phoneNumber: '33612345678', friendlyName: '33612345678', createdAt: '2022-09-21' },
         { type: 'email', email: 'root@reach5.co', friendlyName: 'identifier', createdAt: '2022-09-21' }
     ]))
 })
@@ -118,7 +118,7 @@ describe('DOM testing', () => {
 
         test('with credentials', async () => {
             listMfaCredentials.mockResolvedValue({ credentials: [
-                { type: 'sms', phoneNumber: '33612345678', friendlyName: 'identifier', createdAt: '2022-09-21' } as MFA.PhoneCredential,
+                { type: 'sms', phoneNumber: '33612345678', friendlyName: '33612345678', createdAt: '2022-09-21' } as MFA.PhoneCredential,
                 { type: 'email', email: 'root@reach5.co', friendlyName: 'identifier', createdAt: '2022-09-21' } as MFA.EmailCredential
             ]})
             await generateComponent({}, defaultConfig);
