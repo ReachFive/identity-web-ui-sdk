84fe007dc8a860a5be4523d935e71d52b92fce4b
sacha <sacha.memmi@reach5.co>
2025-09-15 16:50:58 +0200

    modifications comments

--- src/components/form/passwordSignupFormComponent.tsx
+++ src/components/form/passwordSignupFormComponent.tsx
@@ -144,12 +144,7 @@ export const PasswordSignupForm = ({
                     ...phoneNumberOptions,
                 }}
                 handler={callback}
-                onSuccess={authResult => {
-                    if(authResult != undefined && authResult.accessToken == undefined && authResult.code == undefined) {
-                        onSuccess({ name: 'signup', authResult, isIdentifierVerificationRequired: true });
-                    }
-                    onSuccess({ name: 'signup', authResult, isIdentifierVerificationRequired: false });
-                }}
+                onSuccess={authResult => onSuccess({ name: 'signup', authResult, isIdentifierVerificationRequired: authResult?.accessToken == undefined && authResult?.code == undefined })}
                 onError={onError}
             />
         </CaptchaProvider>
